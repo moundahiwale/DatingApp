9eae37ab77e6abf408527077398d0d4215213142
moundahiwale <moun.dahiwale@liberty-it.c.o.uk>
2018-08-07 21:19:30 +0100

    Error Handling in Angular

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log('Logged in successfully');
     }, error => {
-      console.log('Login failed');
+      console.log(error);
     });
   }
 
