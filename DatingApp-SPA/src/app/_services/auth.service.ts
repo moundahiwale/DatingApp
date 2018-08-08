201c809ad13531c4b4220f03802d333e8727051d
moundahiwale <moun.dahiwale@liberty-it.c.o.uk>
2018-08-08 10:03:49 +0100

    Added Angular JWT library to improve token handling and decode tokens

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,13 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private baseURL = 'http://localhost:5000/api/auth/';
+  private jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   login(model: any) {
     return this.http.post(this.baseURL + 'login', model).pipe(
@@ -15,6 +19,7 @@ export class AuthService {
         const user = response;
         if (user) {
           localStorage.setItem('token', user.token);
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
         }
       })
     );
@@ -22,5 +27,10 @@ export class AuthService {
 
   register(model: any) {
     return this.http.post(this.baseURL + 'register', model);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 }
